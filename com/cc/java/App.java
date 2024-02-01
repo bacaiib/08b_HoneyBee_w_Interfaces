46f8f237e35766e77230f133766c41b2b270dd88
bacaiib <roman-frommhold@gmx.net>
2024-02-01 10:56:41 +0100

    Fertige änderung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,5 +1,10 @@
 package com.cc.java;
 
+import com.cc.java.bees.Drone;
+import com.cc.java.bees.HoneyBee;
+import com.cc.java.bees.Queen;
+import com.cc.java.bees.Worker;
+import com.cc.java.birds.Bird;
 public class App {
     
     public static void main(String[] args) {
@@ -7,10 +12,12 @@ public class App {
        Queen queen = new Queen();
        Worker worker = new Worker();
        Drone drone = new Drone();
+       Bird bird = new Bird();       
        
        pollObj(queen);
        pollObj(worker);
        pollObj(drone);
+       pollObj(bird);
 
         //-- Das geht nicht mehr - ABSTRAKTION!--//
         // HoneyBee bee = new HoneyBee();
@@ -24,6 +31,14 @@ public class App {
         output(obj.fly());
         output("-----------------");
     }
+
+    // --- Überladene Methode Bird(s) ---//
+    private static void pollObj(Bird obj){
+        output(obj.hasFeathers());
+        output(obj.fly());
+        output("-----------------");
+    }
+
 
 
     private static void output(String outputStr) {
---
+++ com/cc/java/birds/Bird.java
@@ -0,0 +1,22 @@
+package com.cc.java.birds;
+
+import com.cc.java.interfaces.Flyable;
+import com.cc.java.interfaces.IFeathers;
+
+public class Bird implements Flyable,IFeathers {
+
+    @Override
+    public String fly() {
+        return "I'm a bird, i can fly!";
+    }
+
+    @Override
+    public String hasFeathers() {
+        return "I'm a bird, i have feathers!";
+    }
+
+    
+
+    
+
+}
